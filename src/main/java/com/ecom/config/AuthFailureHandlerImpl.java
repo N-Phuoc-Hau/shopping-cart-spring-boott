5dd6fa8846e889873388e1c6a2dc53a049b7d0f1
phuoc hau <binpe09x@gmail.com>
2024-11-05 17:39:45 +0700

    sửa đổi phần Failure
    Chia failure ra 2 phần cho dễ nhìn để đưa vào Báo cáo

--- src/main/java/com/ecom/config/AuthFailureHandlerImpl.java
+++ src/main/java/com/ecom/config/AuthFailureHandlerImpl.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Component;
 import com.ecom.model.UserDtls;
 import com.ecom.repository.UserRepository;
 import com.ecom.service.UserService;
-import com.ecom.service.impl.UserServiceImpl;
 import com.ecom.util.AppConstant;
 
 import jakarta.servlet.ServletException;
@@ -21,50 +20,48 @@ import jakarta.servlet.http.HttpServletResponse;
 @Component
 public class AuthFailureHandlerImpl extends SimpleUrlAuthenticationFailureHandler {
 
-	@Autowired
-	private UserRepository userRepository;
+    @Autowired
+    private UserRepository userRepository;
 
-	@Autowired
-	private UserService userService;
+    @Autowired
+    private UserService userService;
 
-	@Override
-	public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
-			AuthenticationException exception) throws IOException, ServletException {
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
+                                        AuthenticationException exception) throws IOException, ServletException {
 
-		String email = request.getParameter("username");
+        String email = request.getParameter("username");
+        UserDtls userDtls = userRepository.findByEmail(email);
 
-		UserDtls userDtls = userRepository.findByEmail(email);
+        if (userDtls != null) {
+            if (userDtls.getIsEnable()) {
+                handleFailedAttempt(userDtls, exception);
+            } else {
+                exception = new LockedException("Your account is inactive");
+            }
+        } else {
+            exception = new LockedException("Invalid email or password");
+        }
 
-		if (userDtls != null) {
+        // Redirect to failure URL with the error message
+        super.setDefaultFailureUrl("/signin?error");
+        super.onAuthenticationFailure(request, response, exception);
+    }
 
-			if (userDtls.getIsEnable()) {
-
-				if (userDtls.getAccountNonLocked()) {
-
-					if (userDtls.getFailedAttempt() < AppConstant.ATTEMPT_TIME) {
-						userService.increaseFailedAttempt(userDtls);
-					} else {
-						userService.userAccountLock(userDtls);
-						exception = new LockedException("Tài khoản của bạn đã bị khóa!! lần thử 3 thất bạ");
-					}
-				} else {
-
-					if (userService.unlockAccountTimeExpired(userDtls)) {
-						exception = new LockedException("Tài khoản của bạn đã được mở khóa !! Hãy thử đăng nhập");
-					} else {
-						exception = new LockedException("tài khoản của bạn đã bị khóa!! Thỉnh thoảng hãy thử lại");
-					}
-				}
-
-			} else {
-				exception = new LockedException("tài khoản của bạn không hoạt động");
-			}
-		} else {
-			exception = new LockedException("Email và mật khẩu không hợp lệ");
-		}
-
-		super.setDefaultFailureUrl("/signin?error");
-		super.onAuthenticationFailure(request, response, exception);
-	}
-
+    private void handleFailedAttempt(UserDtls userDtls, AuthenticationException exception) {
+        if (userDtls.getAccountNonLocked()) {
+            if (userDtls.getFailedAttempt() < AppConstant.ATTEMPT_TIME) {
+                userService.increaseFailedAttempt(userDtls);
+            } else {
+                userService.userAccountLock(userDtls);
+                exception = new LockedException("Your account is locked due to 3 failed attempts");
+            }
+        } else {
+            if (userService.unlockAccountTimeExpired(userDtls)) {
+                exception = new LockedException("Your account is now unlocked. Please try to login again.");
+            } else {
+                exception = new LockedException("Your account is locked. Please try again later.");
+            }
+        }
+    }
 }
